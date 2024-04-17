6e3004ffe181bbf3c17cdbe2af490b627c91f866
Malik <malikhussein229@gmail.com>
2024-04-17 16:34:20 -0700

    Task#9

--- models/place.py
+++ models/place.py
@@ -18,3 +18,16 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, nullable=False, default=0)
     latitude = Column(Float)
     longitude = Column(Float)
+
+    reviews = relationship('Review', backref='place', cascade='delete')
+
+    if getenv("HBNB_TYPE_STORAGE", None) != "db":
+        @property
+        def reviews(self):
+            """Getter method for reviews attribute for FileStorage"""
+            from models.review import Review
+            review_list = []
+            for review in list(models.storage.all(Review).values()):
+                if review.place_id = self.id:
+                    review_list.append(review)
+            return review
