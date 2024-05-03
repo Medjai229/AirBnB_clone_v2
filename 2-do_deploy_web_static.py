38d92cc0f80ab1cb5bb5608a4ee464d0f8163f8d
Malik <malikhussein229@gmail.com>
2024-05-03 06:20:42 -0700

    Task#2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -27,18 +27,42 @@ def do_deploy(archive_path):
     if exists(archive_path) is False:
         return False
 
-    try:
-        file_name = archive_path.split("/")[-1]
-        no_ext = file_n.split(".")[0]
-        path = "/data/web_static/releases/"
-        put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_name, path, no_ext))
-        run('rm /tmp/{}'.format(file_name))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
-        return True
-    except:
+    full_name = archive_path.split("/")[1]
+    file_name = archive_path.split("/")[1],split(".")[0]
+
+    if put(archive_path, "/tmp/{}".format(
+           full_name)).failed is True:
         return False
+
+    if run("rm -rf /data/web_static/releases/{}/".format(
+           file_name)).succeeded is False:
+        return False
+
+    if run("mkdir -p /data/web_static/releases/{}/".format(
+           file_name)).succeeded is False:
+        return False
+
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(
+           full_name, file_name)).succeeded is False:
+        return False
+
+    if run("rm /tmp/{}".format(full_name)).failed is True:
+        return False
+
+    if run("mv /data/web_static/releases/{}/web_static/* "
+           "/data/web_static/releases/{}/".format(file_name, file_name)
+           ).failed is True:
+        return False
+
+    if run("rm -rf /data/web_static/releases/{}/web_static".
+           format(file_name)).failed is True:
+        return False
+
+    if run("rm -rf /data/web_static/current").failed is True:
+        return False
+
+    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+           format(file_name)).succeeded is False:
+        return False
+
+    return True
