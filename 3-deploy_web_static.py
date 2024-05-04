16a08a591e4ee859c44fdeccbabd596277a35d7c
Malik <malikhussein229@gmail.com>
2024-05-03 17:35:28 -0700

    Fixing Task#3

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -44,7 +44,7 @@ def do_deploy(archive_path):
 
 def deploy():
     """creates and distributes an archive to the web servers"""
-    archive_path = do_pack()
-    if archive_path is None:
-        return False
-    return do_deploy(archive_path)
+    file_path = do_pack()
+    if file_path is not None:
+        return do_deploy(file_path)
+    return False
