513ee6fff02cbcab0e9f78c6d79c27eeb011278b
Malik <malikhussein229@gmail.com>
2024-05-19 11:33:37 -0700

    Task#8

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""This script starts a Flask web application"""
+from models import storage
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """renders an html page with listed states"""
+    states = storage.all("State")
+    return render_template("7-states_list.html", states=states)
+
+
+@app.teardown_appcontext
+def teardown_db(execute):
+    """remove the current SQLAlchemy Session"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
