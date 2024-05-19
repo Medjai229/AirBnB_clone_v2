9414e9fbef937338b544f8c15856aaf5eaf67963
Medjai229 <115402766+Medjai229@users.noreply.github.com>
2024-05-19 20:52:24 +0300

    Update state.py

--- models/state.py
+++ models/state.py
@@ -9,18 +9,24 @@ from os import getenv
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-    cities = relationship('City', backref='state', cascade="all, delete")
+    if models.storage_t == "db":
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state")
+    else:
+        name = ""
 
-    if getenv("HBNB_TYPE_STORAGE") != 'db':
+    def __init__(self, *args, **kwargs):
+        """initializes state"""
+        super().__init__(*args, **kwargs)
+
+    if models.storage_t != "db":
         @property
-        def citites(self):
-            from models import storage
-            from models.city import City
-            """Get a list of all related City objects"""
+        def cities(self):
+            """getter for list of city instances related to the state"""
             city_list = []
-            for city in list(storage.all(City).values()):
+            all_cities = models.storage.all(City)
+            for city in all_cities.values():
                 if city.state_id == self.id:
                     city_list.append(city)
             return city_list
