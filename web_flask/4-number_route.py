1a9b1d50cdd7752dd180a36cc076a4268eca2ca2
Malik <malikhussein229@gmail.com>
2024-05-18 18:09:18 -0700

    Task#4

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -32,7 +32,7 @@ def pythoniscool(text="is cool"):
     """
     return "Python {}".format(text.replace("_", " "))
 
-@app.route("/number/<n>", strict_slashes=False)
+@app.route("/number/<int:n>", strict_slashes=False)
 def numbern(n):
     """return  "n is a number" if n is an int"""
     return "{} is a number".format(n)
