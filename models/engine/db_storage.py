4729166b565d22ff2416f970522aeefb600277dd
Medjai229 <115402766+Medjai229@users.noreply.github.com>
2024-05-19 20:57:30 +0300

    Update db_storage.py

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""This module defines a class to manage db storage for hbnb clone"""
+"""This module Defines the DBStorage engine."""
+
 from os import getenv
 from models.base_model import Base
 from models.base_model import BaseModel
